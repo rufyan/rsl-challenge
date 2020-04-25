6006946af370c8081628af7053f7187e9701f33a
connprosser <conn@guerrilla.com.au>
2020-04-26 07:05:23 +1000

    New Unit tests - api return type and hydrate draws

--- ApiResponseTest/UnitTest1.cs
+++ ApiResponseTest/UnitTest1.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
+using NSubstitute;
 using rsl_challenge.Models;
+using rsl_challenge.Repository;
 using rsl_challenge.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace UnitTests
 {
@@ -9,29 +13,42 @@ namespace UnitTests
     public class ApiResponseTest 
     {
         private readonly ILotteryService _lotteryService;
+        private readonly IDrawRepository _drawRepository;
+        private readonly IConfiguration _config;
+
         public ApiResponseTest()
         {
-        }
-        public ApiResponseTest(ILotteryService LotteryService)
-        {
-            _lotteryService = LotteryService;
-        }
-
-
-        [TestMethod]
-        public void CheckOpenDrawsCount()
-        {
-            var list = new DrawsList();
-            var mockService = new Mock<ILotteryService>();
-            mockService.Setup(x => x.GetOpenDrawList()).Returns(list);
+            _config = Substitute.For<IConfigurationRoot>();
+            _config.GetValue<string>("CompanyId").Returns("Tattersalls");
+            _lotteryService = new LotteryService(_config);
+            _drawRepository = new DrawRepository();
         }
 
         [TestMethod]
-        public void CheckLatestResultsCount()
+        public async Task GetResultsApiAsync_ReturnsDrawsList()
         {
-            var list = new LotteryResultsList();
-            var mockService = new Mock<ILotteryService>();
-            mockService.Setup(x => x.GetLotteryResultsList("")).Returns(list);
+            var result = await _lotteryService.GetApiResultsAsync<DrawsList>($"https://data.api.thelott.com/sales/vmax/web/data/lotto/opendraws", string.Empty);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void HydrateDraws_ReturnsDifferentDrawsList()
+        {
+            //Arrange
+            var testDrawsList = Substitute.For<DrawsList>();
+            testDrawsList.Draws = new List<Draw>() {
+                new Draw() {
+                    DrawLogoUrl = "test",
+                    ProductId = "OzLotto"
+                }
+            };
+
+            //Act
+            var result = _drawRepository.HydrateDraws(testDrawsList);
+
+            //Assert
+            Assert.AreNotEqual(result, testDrawsList);
         }
     }
 }
+
--- rsl-challenge/Services/LotteryService.cs
+++ rsl-challenge/Services/LotteryService.cs
@@ -24,31 +24,31 @@ namespace rsl_challenge.Services
             _companyId = _config.GetValue<string>("CompanyId");
         }
 
-        static readonly HttpClient client = new HttpClient();
+        readonly HttpClient client = new HttpClient();
 
-        public  LotteryResultsList GetLotteryResultsList(string productId) =>
+        public LotteryResultsList GetLotteryResultsList(string productId) =>
             GetApiResultsAsync<LotteryResultsList>($"{_url}/{_config.GetValue<string>("Endpoint:LatestResults")}", productId).GetAwaiter().GetResult();
 
-        public  DrawsList GetOpenDrawList() =>
+        public DrawsList GetOpenDrawList() =>
             GetApiResultsAsync<DrawsList>($"{_url}/{_config.GetValue<string>("Endpoint:OpenDraws")}", string.Empty).GetAwaiter().GetResult();
 
         //Hit the Lott Api
         public async Task<T> GetApiResultsAsync<T>(string path, string productId) where T : class, ILottery
         {
-
-        var apiQuery = new ApiQuery
+            var apiQuery = new ApiQuery
             {
                 CompanyId = _companyId,
                 MaxDrawCountPerProduct = "2",
                 OptionalProductFilter = _defaultProducts
             };
+
             if (!string.IsNullOrEmpty(productId)){
                 apiQuery.OptionalProductFilter = new string[] { productId };
             }
+
             var jsonString = JsonConvert.SerializeObject(apiQuery);
             T results = null;
             var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-
             var client = new HttpClient();
             var response = await client.PostAsync(path, content);
 
